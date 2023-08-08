0b29532ecbd725d546a7d8bdf66400e13120bc99
rushiekshgaikwad413 <rushikeshgaikwad413@gmail.com>
2023-08-08 18:09:48 +0530

    Initial commit

---
+++ spring-boot-OTP-enabled-app-master/src/main/java/com/rushikesh/springbootOTPenabledapp/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rushikesh.springbootOTPenabledapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserPojo, Integer>{
+
+	UserPojo findByUsername(String username);
+
+} 
+
---
+++ spring-boot-OTP-enabled-app-master/src/test/java/com/rushikesh/springbootOTPenabledapp/SpringBootOtpEnabledAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushikesh.springbootOTPenabledapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOtpEnabledAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
